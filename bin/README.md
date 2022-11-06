59e75f4d0187b189888ced296632e54ad2c052e0
GITHUB <priyamail318@gmail.com>
2022-11-06 20:07:49 +0530

    further changes

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# studygit by PRIYA
---
+++ src/org/poly/First.java
@@ -0,0 +1,13 @@
+package org.poly;
+
+public class First {
+	private void one() {
+	System.out.println("one");
+}
+	private void two() {
+		System.out.println("two");
+	}
+	private void three() {
+		System.out.println("three");
+	}
+}
